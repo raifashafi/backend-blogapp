c4c633fece67693ba3bd0ab0ec6849a6fe808ce9
raifashafi <raifafisat@gmail.com>
2024-07-28 16:56:06 +0530

    project created

--- app.js
+++ app.js
@@ -13,6 +13,49 @@ app.use(Cors())
 Mongoose.connect("mongodb+srv://raifashafi:raifashafi@cluster0.tznb7.mongodb.net/blogappDB?retryWrites=true&w=majority&appName=Cluster0")
 
 
+//signin
+app.post("/signIn",async(req,res)=>{
+   let input=req.body
+   let result=userModel.find({email:req.body.email}).then(
+    (items)=>{
+      if (items.length>0) {
+         const passwordValidator=Bcrypt.compareSync(req.body.password,items[0].password)
+         if (passwordValidator) {
+            
+            jwt.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},
+                (error,token)=>{
+                    if (error) {
+                        
+                        res.json({"status":"error","errorMessage":error})
+                    } else {
+                        
+                        res.json({"status":"success","token":token,"userId":items[0]._id})
+                    }
+
+            })
+            
+
+         } else {
+            res.json({"status":"incorrect password"})
+         }
+
+
+
+      } else {
+        res.json({"status":"invalid emailid"})
+      }
+
+
+    }
+   ).catch()
+
+
+})
+
+
+
+
+//usersignup
 app.post("/signUp", async (req, res) => {
     let input = req.body
     let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
